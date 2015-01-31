ded4d48a72ddbd997390b056e17096b8dc94a671
Krunal <krunalamodi@gmail.com>
2015-01-31 00:49:09 -0800

    Test changes
    HelloWorld :)

---
+++ Solutions/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class HelloWorld
+    {
+        public void printHelloWorld(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+        public static void HelloWorldHelper()
+        {
+            HelloWorld obj = new HelloWorld();
+            obj.printHelloWorld(3);
+        }
+    }
+}
---
+++ TestCode/TestCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions;
+
+namespace TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.HelloWorldHelper();
+
+            Console.ReadLine();
+        }
+    }
+}
