719113a40e7fed0d11395203d111e572dbbb47e8
Krunal <krunalamodi@gmail.com>
2015-04-01 22:37:19 -0700

    formatting

--- Solutions/MaximizeOnes.cs
+++ Solutions/MaximizeOnes.cs
@@ -1,11 +1,16 @@
 /*
-There are n integers a1, a2, ..., an. Each of those integers can be either 0 or 1. We are allowed to do exactly one move: We must choose two indices i and j (1 <= i <= j <= n) and flip all values ak for which their positions are in range [i, j] (that is i <= k <= j). Flip the value of x means to apply operation x = 1 - x.
+There are n integers a1, a2, ..., an. Each of those integers can be either 0 or 1.
+We are allowed to do exactly one move: We must choose two indices 
+i and j (1 <= i <= j <= n) and flip all values ak for which their positions are in 
+range [i, j] (that is i <= k <= j). Flip the value of x means to apply operation x = 1 - x.
 
 The goal is - After exactly one move to obtain the maximum number of ones. 
 
 Input:-
 
-The first line of the input contains an integer n (1 <= n <= 10^6). In the second line of the input there are n integers: a1, a2, ..., an. It is guaranteed that each of those n values is either 0 or 1
+The first line of the input contains an integer n (1 <= n <= 10^6).
+In the second line of the input there are n integers: a1, a2, ..., an.
+It is guaranteed that each of those n values is either 0 or 1
 
 Output:-
 Print an integer - the maximal number of 1s that can be obtained after exactly one move.
@@ -24,9 +29,12 @@ output
 4
 
 Note:-
-In the first case, flip the segment from 2 to 5 (i=2, j=5). That flip changes the sequence, it becomes: [1 1 1 0 1]. So, it contains four ones. There is no way to make the whole sequence equal to [1 1 1 1 1].
+In the first case, flip the segment from 2 to 5 (i=2, j=5).
+That flip changes the sequence, it becomes: [1 1 1 0 1].
+So, it contains four ones. There is no way to make the whole sequence equal to [1 1 1 1 1].
 
-In the second case, flipping only the second and the third element (i=2, j=3) will turn all numbers into 1.
+In the second case, flipping only the second and 
+the third element (i=2, j=3) will turn all numbers into 1.
 
 
 Source: http://codeforces.ru/problemset/problem/327/A
@@ -38,7 +46,7 @@ namespace FlippingGame327A
 {
     class Program
     {
-        int kkMax(int a, int b)
+        static int kkMax(int a, int b)
         {
             return (a < b) ? b : a;
         }
